0869c04f1fcb74ce49abb6f760afa0a8a97b9b8d
marcosjunior00 <marcosantonio.jose01@gmail.com>
2026-03-27 21:27:25 -0300

    chore: Initialize backend structure with Maven configuration and Spring Boot application

---
+++ backend/src/test/java/br/com/marcosjunior/crud_usuarios/CrudUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marcosjunior.crud_usuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
